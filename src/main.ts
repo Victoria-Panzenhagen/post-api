4d4870800b2f0fb2dc3a879452fe6e0e82cd09e4
victoria <victoria.lemobs@gmail.com>
2026-06-29 07:13:10 -0300

    docs: configuração inicial

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Post API')
+    .setDescription('API para gerenciamento de posts')
+    .setVersion('1.0')
+    .addTag('posts')
+    // .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove propriedades não definidas no DTO
+      forbidNonWhitelisted: true, // Lança erro se houver propriedades não definidas no DTO
+      transform: true, // Transforma payloads para instâncias de DTO
+    }),
+  );
+
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
